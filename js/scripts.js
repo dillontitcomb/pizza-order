3efe3da4f82675c93b4718c2151fe6215b3fdabb
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-26 12:04:44 -0800

    fixes a number of syntax errors in the pizza.cost method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,47 @@
 //backend logic
+var toppingsArray = [""];
+var pizzaCost = 0;
 
-function Pizza (sauce, toppings, size, cost) {
+function Pizza (size, sauce, toppings) {
+	this.size = size;
 	this.sauce = sauce;
 	this.toppings = toppings;
-	this.size = size;
-	this.cost = cost;
 }
 
-var getToppingsArray = function () {
+var getToppingsArray = function() {
 	$("input:checkbox[name=pizza-toppings]:checked").each(function() {
-		var pizzaTopping = $(this).val();
-		toppingsArray.push(pizzaTopping);
+	var pizzaTopping = $(this).val();
+	toppingsArray.push(pizzaTopping);
 	});
 };
 
-
+Pizza.prototype.cost = function () {
+	alert(this.size);
+	if (this.size === "small") {
+			pizzaCost += 10;
+		} else if (this.size === "medium") {
+			pizzaCost += 13;
+		} else if (this.size === "large") {
+			pizzaCost += 16;
+	}
+	if (this.sauce === "pesto") {
+			pizzaCost += 2;
+		} else if (this.sauce === "evoo") {
+			pizzaCost += 3
+	}
+}
 
 //frontend logic
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
 		event.preventDefault();
-		var toppingsArray = [];
 		inputSize = $("#pizza-size").val();
 		inputSauce = $("#pizza-sauce").val();
 		getToppingsArray();
-		alert("hi!");
+		var newPizza = new Pizza(inputSize, inputSauce, toppingsArray);
+		newPizza.cost();
+		console.log(newPizza);
+		alert(pizzaCost);
 		alert(inputSize + inputSauce + toppingsArray);
 	});
 });
-
-
-
-		// var newPizza = new Pizza(inputSauce, arrayToppings, inputSize, calculatedCost);
