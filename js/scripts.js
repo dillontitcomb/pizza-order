cf0a9323c6d27116c2f8ee44e3d5b8bb354e3878
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-26 11:12:58 -0800

    Adds functioning backend logic that returns pizza size, sauce base, and array of selected toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-// //backend logic
-//
-// function Pizza (sauce, toppings, size, cost) {
-// 	this.sauce = sauce;
-// 	this.toppings = toppings;
-// 	this.size = size;
-// 	this.cost = cost;
-// }
-//
-//
-//
-//
-// //frontend logic
-// $(document).ready(function() {
-//   $("form#pizza-order").submit(function(event) {
-// 		event.preventDefault();
-//
-//
-// 		var newPizza = new Pizza(inputSauce, arrayToppings, inputSize, calculatedCost);
+//backend logic
+
+function Pizza (sauce, toppings, size, cost) {
+	this.sauce = sauce;
+	this.toppings = toppings;
+	this.size = size;
+	this.cost = cost;
+}
+
+var getToppingsArray = function () {
+	$("input:checkbox[name=pizza-toppings]:checked").each(function() {
+		var pizzaTopping = $(this).val();
+		toppingsArray.push(pizzaTopping);
+	});
+};
+
+
+
+//frontend logic
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+		event.preventDefault();
+		var toppingsArray = [];
+		inputSize = $("#pizza-size").val();
+		inputSauce = $("#pizza-sauce").val();
+		getToppingsArray();
+		alert("hi!");
+		alert(inputSize + inputSauce + toppingsArray);
+	});
+});
+
+
+
+		// var newPizza = new Pizza(inputSauce, arrayToppings, inputSize, calculatedCost);
